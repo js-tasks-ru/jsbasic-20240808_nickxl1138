d01dedeeea4cf99a31453fe8b44376d903688b3c
NickXL1138 <nickxl1138@gmail.com>
2024-08-17 10:54:36 +0700

    Занятие 2 написаны функции

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,18 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (key in salaries) {
+    let badNumberCheck = (typeof salaries[key] != "number" 
+        || isNaN(salaries[key]) 
+        || salaries[key] == Infinity 
+        || salaries[key] == -Infinity);
+
+    if (badNumberCheck == true) {
+      salaries[key] = 0;
+    }
+    
+    sum += salaries[key]; 
+
+  }
+  return sum;
 }
