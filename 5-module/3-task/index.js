121086eb0a554b31c573e568f6591f988369b2b2
NickXL1138 <nickxl1138@gmail.com>
2024-08-29 20:06:31 +0700

    Module 5 task 1-3 написаны функции

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
-  // ваш код...
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let slides = document.querySelector('.carousel__inner');
+  let slideWidthSum = 0;
+
+  arrowLeft.style.display = 'none';
+
+  arrowRight.onclick = function () {
+    arrowLeft.style.display = '';
+    slideWidthSum += slides.offsetWidth;
+    slides.style.transform = `translateX(${-slideWidthSum}px)`;
+    
+    if (slideWidthSum >= slides.offsetWidth * 3) {
+      arrowRight.style.display = 'none';
+    }
+  };
+
+  arrowLeft.onclick = function () {
+    arrowRight.style.display = '';
+    slideWidthSum -= slides.offsetWidth;
+    slides.style.transform = `translateX(${-slideWidthSum}px)`;
+    
+    if (slideWidthSum <= 0) {
+      arrowLeft.style.display = 'none';
+    }
+  };
 }
