e969d6ef4b66082a35a32dfd04a722294784f346
NickXL1138 <nickxl1138@gmail.com>
2024-09-10 12:53:37 +0700

    Занятие 7 задание 1-3 созданы классы задание 4 модифицирован класс

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,107 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class RibbonMenu {
+  elem = null;
+  #categories = [];
+
   constructor(categories) {
-    this.categories = categories;
+    this.#categories = categories;
+    this.#menuAssembly();
+    this.#addEventListeners();
+  }
+
+  #menuAssembly() {
+    this.elem = createElement(`
+      <div class="ribbon">
+        <button class="ribbon__arrow ribbon__arrow_left">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+        <nav class="ribbon__inner">  
+        </nav>
+        <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+      </div>
+    `);
+
+    const menuItem = this.#categories.map(category => createElement(`
+      <a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>
+    `));
+
+    this.#ribbonSelect('inner').append(...menuItem);
+
+    const ribbonActive = this.#ribbonSelect('inner').firstElementChild;
+    ribbonActive.classList.add('ribbon__item_active');
+  }
+
+  
+  #ribbonSelect(sel) {
+    return this.elem.querySelector(`.ribbon__${sel}`);
+  }
+
+  #addEventListeners() {
+    this.elem.onclick = (click) => {
+      if (click.target.closest('.ribbon__item')) {
+        this.#categorySelect(click);
+      }
+
+      if (click.target.closest('.ribbon__arrow_right')) {
+        this.#right(); 
+      }
+
+      if (click.target.closest('.ribbon__arrow_left')) {
+        this.#left();
+      }
+    };
+    this.#ribbonSelect('inner').onscroll = this.#actionOnButton;
+  }
+
+  #categorySelect(click) {
+    click.preventDefault();
+
+    const ribbonItem = click.target.closest('[data-id]');
+    const allItems = this.elem.querySelectorAll('.ribbon__item_active');
+
+    for (let item of allItems) {
+      item.classList.remove('ribbon__item_active');
+    }
+
+    ribbonItem.classList.add('ribbon__item_active');
+
+    const ribbonEvent = new CustomEvent('ribbon-select', {
+      detail: ribbonItem.dataset.id,
+      bubbles: true
+    });
+    
+    this.elem.dispatchEvent(ribbonEvent);
+  }
+
+  #right() {
+    this.#ribbonSelect('inner').scrollBy(350, 0);
+    this.#actionOnButton();
+  }
+
+  #left() {
+    this.#ribbonSelect('inner').scrollBy(-350, 0);
+    this.#actionOnButton();
+  }
+
+  #actionOnButton = () => {
+    const scrollLeft = this.#ribbonSelect('inner').scrollLeft;
+    const scrollWidth = this.#ribbonSelect('inner').scrollWidth;
+    const clientWidth = this.#ribbonSelect('inner').clientWidth;
+    const scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+    if (scrollLeft > 0) {
+      this.#ribbonSelect('arrow_left').classList.add('ribbon__arrow_visible');
+    } else {
+      this.#ribbonSelect('arrow_left').classList.remove('ribbon__arrow_visible');
+    }
+
+    if (scrollRight > 0) {
+      this.#ribbonSelect('arrow_right').classList.add('ribbon__arrow_visible');
+    } else {
+      this.#ribbonSelect('arrow_right').classList.remove('ribbon__arrow_visible');
+    }
   }
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,67 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
+  elem = null;
+  
   constructor() {
+    this.#makeModal();
+    this.#addEventListeners();
+  }
+
+  #makeModal () {
+    this.elem = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title">
+            </h3>
+          </div>
+          <div class="modal__body">
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  #addEventListeners() {
+    this.#modalSelect('close').onclick = () => {
+      this.close();
+    };
+    document.addEventListener('keydown', this.#keydown);
+  }
+  
+  #modalSelect(sel) {
+    return this.elem.querySelector(`.modal__${sel}`);
+  }
+
+  #keydown = (key) => {
+    if (key.code == 'Escape') {
+      this.close();
+    }
+  }
+
+  open() {
+    document.body.append(this.elem);
+    document.body.classList.add('is-modal-open');
+  }
+
+  close() {
+    this.elem.remove();
+    document.body.classList.remove('is-modal-open');
+    document.removeEventListener('keydown', this.#keydown);
+  }
+
+  setTitle(title) {
+    this.#modalSelect('title').textContent = title;
+  }
+
+  setBody(body) {
+    this.#modalSelect('body').innerHTML = '';
+    this.#modalSelect('body').append(body);
   }
 }
+
